ce04ae44288818ee521e9790b40014945033f58e
Pre-11 <prernakumari7427@gmail.com>
2023-02-10 13:50:55 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Counter from './component/Counter';
+import Todo from './component/Todo';
 
 function App() {
   return (
     <div className="App">
       <Counter />
+      <Todo />
 
     </div>
   );
---
+++ src/component/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Todo() {
+  return (
+    <div>
+        <input placeholder="Enter Text"/>
+        <button>Submit</button>
+    </div>
+  )
+}
+
+export default Todo
