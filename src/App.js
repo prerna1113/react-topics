5fa8a5fbc387afa17d38f47c4de6885e06d0aacd
Pre-11 <prernakumari7427@gmail.com>
2023-02-13 13:13:29 +0530

    table component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 
+import { useState } from 'react';
 import './App.css';
 import Card from './component/Card';
 import Counter from './component/Counter';
+import Table from './component/Table';
 import Todo from './component/Todo';
 
 function App() {
+  const [state,setState]= useState("true");
   return (
     <div className="App">
-      <Card />
+     <div style={{display:"flex",justifyContent:"space-evenly",
+     margin:"auto",
+     padding:"40px"
+     }}>
+       <Card />
+      <Table />
+     
+      </div>
+      <button onClick={()=>{
+        setState("false")
+      }}>show card</button>
+      <button>show Table</button>
       {/* <Counter />
       <Todo /> */}
 
--- src/component/Table.js
+++ src/component/Table.js
@@ -2,7 +2,35 @@ import React from 'react'
 
 function Table() {
   return (
-    <div>Table</div>
+    <div >
+        <table style={{width:"500px"}} border ="1">
+        <thead>
+            <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>City</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td>101</td>
+            <td>Prerna</td>
+            <td>Jaipur</td>
+            </tr>
+            <tr>
+            <td>102</td>
+            <td>Prerna</td>
+            <td>Jaipur</td>
+            </tr>
+            <tr>
+            <td>103</td>
+            <td>Prerna</td>
+            <td>Jaipur</td>
+            </tr>
+        </tbody>
+        </table>
+        
+    </div>
   )
 }
 
