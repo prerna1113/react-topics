1ac4a436cab9858313378c58ee359e58dd5a517f
Pre-11 <prernakumari7427@gmail.com>
2023-04-07 12:08:28 +0530

    completed

--- src/component/Api/Profile.jsx
+++ src/component/Api/Profile.jsx
@@ -9,6 +9,12 @@ function Profile() {
         Gender:"Male",
         Image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4MI6FjdCs4QjREvY7BfcHGS29pDWVMrSD9A&usqp=CAU",
     })
+    const [styles,setStyles]=useState({
+        bgColor:"white",
+        color:"black",
+        text:"check to switch dark mode"
+    });
+
     const getRandomUser= () => {
         fetch("https://randomuser.me/api/?results=1")
         .then((res)=>res.json())
@@ -18,20 +24,40 @@ function Profile() {
                 name:userData.results[0].name.first,
                 City:userData.results[0].location.city,
                 Gender:userData.results[0].gender,
-                Image:userData.results[0].picture.medium,
+                Image:userData.results[0].picture.large,
             });
         })
         
     }
 
+    
+    const ChangeStyle=(event)=>{
+        if(event.target.checked){
+            setStyles({
+                bgColor:"black",
+                color:"white",
+                text:"Uncheck to switch light mode"
+            })
+        }
+        else{
+            setStyles({
+                bgColor:"white",
+        color:"black",
+        text:"check to switch dark mode"
+            })
+            
+        }
+    }
+
   return (
-    <div className="profile">
-        <div style={{width:"35%"}}> 
+    <div className="profile" 
+    style={{backgroundColor:styles.bgColor,color:styles.color}}>
+        <div style={{width:"40%"}}> 
             <img src={state.Image} width="100%" height="100%"  alt="Profile Image"/>
 
         </div>
 
-        <div style={{width:"65%" ,textAlign:"left",padding:"10px"}}>
+        <div style={{width:"50%" ,textAlign:"left",padding:"10px"}}>
             <dl>
                 <dt><b>User Name</b></dt>
                 <dd>{state.name}</dd>
@@ -52,6 +78,9 @@ function Profile() {
                 <dd>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia excepturi expedita fuga consectetur rem optio maxime fugit, nihil culpa atque temporibus! Consectetur enim, eos asperiores illo odio minus labore aperiam?</dd>
             </dl>
         <button onClick={getRandomUser}>Random User</button>
+        <br></br>
+        <input type="checkbox" style={{marginTop:"14px"}} onChange={ChangeStyle}/>
+ {styles.text}
         </div>
     </div>
   )
