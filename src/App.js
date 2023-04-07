fcee18076dcdd456f2fb583a593cb15d828ede58
Pre-11 <prernakumari7427@gmail.com>
2023-04-07 13:53:53 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 
 import './App.css';
+import Navbar from './component/Api/Navbar';
+import nav from './component/Api/Navbar';
+import Profile from './component/Api/Profile';
+import Input from './component/Parent-Child/Input';
 
 // import Counterr from './Counterr';
-import Nav from './component/State-Api/Nav';
-import Profile from './component/State-Api/Profile';
+// import Nav from './component/State-Api/Nav';
+// import Profile from './component/State-Api/Profile';
 
 
 
@@ -18,9 +22,12 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar/>
+      <Input />
+      {/* <Profile/> */}
       {/* <Counterr/> */}
-      <Nav/>
-      <Profile />
+      {/* <Nav/>
+      <Profile /> */}
       
     </div>
   );
