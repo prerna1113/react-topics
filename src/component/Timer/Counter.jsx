9215ef7192995e18a1dbad1d2ac7828a882a921e
Pre-11 <prernakumari7427@gmail.com>
2023-04-10 13:41:41 +0530

    added setInterval

--- src/component/Timer/Counter.jsx
+++ src/component/Timer/Counter.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import {useState} from 'react'
 import styles from './Counter.module.css'
 
 function Counter() {
+  const[state,setState]= useState(0);
+
+  const IncreaseValue=()=>{
+    setInterval(()=>{
+
+      setState((prevState)=>prevState+1)
+    },1000)
+  }
+
+
+  const StopValue=()=>{
+    
+  }
   return (
     <div className={styles.timer}>
-        <h1>Timer is 0</h1>
-        <button>start</button> &nbsp; &nbsp;
-        <button>stop</button>
+        <h1>Timer is {state}</h1>
+        <button onClick={IncreaseValue}>start</button> &nbsp; &nbsp;
+        <button onClick={StopValue}>stop</button>
     </div>
   )
 }
