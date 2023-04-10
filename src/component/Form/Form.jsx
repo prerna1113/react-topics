dff3d340dd441764f750dfce1880f46b31d990c6
Pre-11 <prernakumari7427@gmail.com>
2023-04-10 15:39:45 +0530

    uncontrolled component

--- src/component/Form/Form.jsx
+++ src/component/Form/Form.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
+import {useRef} from 'react'
 
 function Form() {
-    var formStyle={
+    const nameRef=useRef(null);
+    const emailRef=useRef(null);
+    const passwordRef=useRef(null);
+        var formStyle={
         width:"300px",
         margin:" 20px auto",
         height:"150px",
         padding:"20px",
         boxShadow:"0 0 10px red",
     }
+    const submitData=()=>{
+        console.log(nameRef.current.value,emailRef.current.value,passwordRef.current.value)
+    }
   return (
     <div style={formStyle}>
        <form>
         <div>
-            <input type="text" placeholder="Enter Name" />
+            <input type="text" placeholder="Enter Name"  ref={nameRef}/>
         </div>
         <br></br>
         <div>
-            <input type="text" placeholder="Enter Email" />
+            <input type="text" placeholder="Enter Email"  ref={emailRef}/>
         </div>
         <br></br>
         <div>
-            <input type="text" placeholder="Enter Password" />
+            <input type="text" placeholder="Enter Password"  ref={passwordRef}/>
         </div>
         <br></br>
         <div>
-            <input type="button" value="Register" />
+            <input type="button" value="Register"  onClick={submitData}/>
         </div>
        </form>
     </div>
