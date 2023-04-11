cb1fd3e93c066c2488f991526335a213521c2232
Pre-11 <prernakumari7427@gmail.com>
2023-04-11 21:50:26 +0530

    added state

--- src/component/REVISION/ParentChild.jsx
+++ src/component/REVISION/ParentChild.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import {useState} from 'react'
 import Text from './Text'
 
-function ParentChild() {
+function ParentChild(props) {
+  const [state,setState]=useState("no data");
+  const submitData=()=>{
+    
+  }
   return (
-    <div style={{width:"300px",margin:"50px auto",padding:"50px",boxShadow:"0 0 10px blue"}}>
-      <input type="Text" placeholder="Enter Some Text" />
+    <div style={{width:"300px",margin:"50px auto",padding:"50px",boxShadow:"0 0 10px blue"
+    
+    }}>
+      <input type="Text" placeholder="Enter Some Text" 
+       onChange={(e)=>{setState(e.target.value)}}
+       />
       <br></br>
       <br></br>
-      <button>Submit</button>
-      <Text />
+      <button onClick={submitData}>Submit</button>
+      <Text data={state}/>
       
     </div>
   )
--- src/component/REVISION/Text.jsx
+++ src/component/REVISION/Text.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Text() {
+function Text({data}) {
   return (
     <div style={{width:"300px",boxShadow:"0 0 10px red",margin:"70px auto"}}>
         <h2>Text Component</h2>
 
-        <p>No data as of Now</p>
+        <p>{data}</p>
     </div>
   )
 }
