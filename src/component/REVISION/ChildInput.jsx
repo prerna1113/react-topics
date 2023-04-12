78d875cc54625b9f7a2028f68374ec236be0dd2a
Pre-11 <prernakumari7427@gmail.com>
2023-04-12 11:33:09 +0530

    added

--- src/component/REVISION/ChildInput.jsx
+++ src/component/REVISION/ChildInput.jsx
@@ -1,14 +1,15 @@
 import {useState} from 'react'
 
 function ChildInput(props) {
-  const [data,setData]=useState();
+  const [input,setInput]=useState();
   const getData=()=>{
-   props.sendData(data);
+    props.sendData(input);
+
   }
   return (
     <div>
-      <input type="text" placeholder="Enter some text" 
-      onChange={(e)=>{setData(e.target.value)}} />
+      <input type="text" placeholder="Enter some text"
+      onChange={(e)=>{setInput(e.target.value)}} />
       <br></br>
       <br></br>
       <button onClick={getData}>Submit</button>
--- src/component/REVISION/ChildText.jsx
+++ src/component/REVISION/ChildText.jsx
@@ -1,14 +1,15 @@
 import {useState} from 'react'
-import ChildInput from './ChildInput'
+import ChildInput from './ChildInput';
 
 function ChildText() {
-  const [state,setState]=useState("No data as of now");
+  const [state,setState] = useState("No data as of now");
   return (
     <div>
-        <h2>Text Component</h2>
-        <p>{state}</p>
-        <ChildInput sendData={setState} />
+      <h2>Text Component</h2>
+      <p>{state}</p>
+      <ChildInput sendData={setState}/>
     </div>
+   
   )
 }
 
