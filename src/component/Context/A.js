9b1e2ab64e0c1bde263cf6834226fa7053ec8252
Pre-11 <prernakumari7427@gmail.com>
2023-04-12 13:01:35 +0530

    added component

---
+++ src/component/Context/A.js
@@ -0,0 +1,16 @@
+import {useState} from 'react'
+
+function A() {
+    const [state,setState]=useState({
+        name:"Chris",
+        City:"Sydney",
+        email:"chris@gmail.com"
+    })
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default A
